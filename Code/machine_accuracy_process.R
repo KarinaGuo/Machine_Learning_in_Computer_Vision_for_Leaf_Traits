1f9db359d9abe8e0d1c421812c6332ac01c19eb7
KarinaGuo <100897883+KarinaGuo@users.noreply.github.com>
2022-07-18 14:43:23 +1000

    Update machine_accuracy_process.R
    Making results more generalisable

--- Code/machine_accuracy_process.R
+++ Code/machine_accuracy_process.R
@@ -1,20 +1,23 @@
 ###
-#validation_meta_dir = "~/Uni/Honours/Thesis/Data Analysis or Code/Data/Labelling List/Validation_list.csv"
-#model_summary_dir = "~/Uni/Honours/Thesis/Data Analysis or Code/Data/model_summary.csv"
-#model_matches_dir = "~/Uni/Honours/Thesis/Data Analysis or Code/Data/model_matches.csv"
+# CODE DOESNT MAP LABELS YET
+test_meta_dir = "C:/Users/swirl/OneDrive - UNSW/Honours/Thesis/Data Analysis or Code/Data/Labelling List/Validation_list.csv"
+model_summary_dir = "C:/Users/swirl/OneDrive - UNSW/Honours/Thesis/Data Analysis or Code/Data/model_summary.csv"
+model_matches_dir = "C:/Users/swirl/OneDrive - UNSW/Honours/Thesis/Data Analysis or Code/Data/model_matches.csv"
+labels = list("Leaf100", "Leaf90", "Leaf50")
 #test = "Two"
 
-#machine_to_accuracy(test, validation_meta_dir, model_summary_dir, model_matches_dir)
+machine_to_accuracy("THREE", test_meta_dir, model_summary_dir, model_matches_dir, labels)
 
-machine_to_accuracy <- function(test, validation_meta_dir, model_summary_dir, model_matches_dir) {
+machine_to_accuracy <- function(test, test_meta_dir, model_summary_dir, model_matches_dir) {
   library (tidyverse)
-  library(ggpubr)
+  library (ggpubr)
   
   dir.create(path = paste0(getwd(), "/Machine_accuracy_results/Test ",paste0(test)))
   
-  validation_meta <- read.csv(validation_meta_dir) %>% 
-    select (id, Leaf100, Leaf100B, Leaf90, Leaf50, Leaf50UM, decimalLongitude, decimalLatitude) %>% 
-    filter (!is.na(Leaf100B))
+#labels here
+  test_meta <- read.csv(test_meta_dir) %>% 
+    select (id, !!!labels, decimalLongitude, decimalLatitude) %>% 
+    filter (!is.na(Leaf90))
   
   model_summary <-read.csv(model_summary_dir)
   
@@ -23,81 +26,73 @@ machine_to_accuracy <- function(test, validation_meta_dir, model_summary_dir, mo
     select (gt_cat_id, pr_cat_id, mask_iou)
   model_matches_2 <- read.csv(file = model_matches_dir) %>% 
     select(mask_iou, file_name, gt_size_px, pr_size_px, ind_gt, ind_pr)
-  model_matches_1[model_matches_1 == "0"] <- "Leaf100"
-  model_matches_1[model_matches_1 == "1"] <- "Leaf100B"
-  model_matches_1[model_matches_1 == "2"] <- "Leaf90"
-  model_matches_1[model_matches_1 == "3"] <- "Leaf50"
-  model_matches_1[model_matches_1 == "4"] <- "Leaf100UM"
-  model_matches_1[model_matches_1 == "5"] <- "Leaf100BUM"
-  model_matches_1[model_matches_1 == "6"] <- "Leaf90UM"
-  model_matches_1[model_matches_1 == "7"] <- "Leaf50UM"
+  model_matches_1[model_matches_1 == "0"] <- labels[1]
+  model_matches_1[model_matches_1 == "1"] <- labels[2]
+  model_matches_1[model_matches_1 == "2"] <- labels[3]
+  #model_matches_1[model_matches_1 == "3"] <- labels[4]
   model_matches <- left_join(model_matches_1, model_matches_2, by="mask_iou")
   .GlobalEnv$model_matches <- model_matches
   
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "0"] <- "Leaf100"
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "1"] <- "Leaf100B"
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "2"] <- "Leaf90"
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "3"] <- "Leaf50"
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "4"] <- "Leaf100UM"
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "5"] <- "Leaf100BUM"
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "6"] <- "Leaf90UM"
-  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "7"] <- "Leaf50UM"
+  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "0"] <- labels[1]
+  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "1"] <- labels[2]
+  model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "2"] <- labels[3]
+  #model_summary["pr_cat_id"][model_summary["pr_cat_id"] == "3"] <- labels[4]
   
   ###Plotting 
   #Counting all categories
   
   .GlobalEnv$model_summary_plot <- model_summary %>% 
     count(pr_cat_id)
-  model_summary_plot[nrow(model_summary_plot) + 1,] <- c("Leaf100", 0)
-  model_summary_plot[nrow(model_summary_plot) + 1,] <- c("Leaf90", 0)
+  #model_summary_plot[nrow(model_summary_plot) + 1,] <- c("Leaf100", 0)
+  #model_summary_plot[nrow(model_summary_plot) + 1,] <- c("Leaf90", 0)
   model_summary_plot <- transform(model_summary_plot, n=as.numeric(n))
   
-  model_summary_plot$pr_cat_id <- factor(model_summary_plot$pr_cat_id, levels=c("Leaf100", "Leaf100UM", "Leaf100B", "Leaf100BUM", "Leaf90", "Leaf90UM", "Leaf50", "Leaf50UM"))
+  model_summary_plot$pr_cat_id <- factor(model_summary_plot$pr_cat_id, levels=c(labels[1], labels[2], labels[3]#, labels[4]
+                                                                                ))
   
   p1 <- ggplot(model_summary_plot, aes(x=pr_cat_id, y=n, fill=pr_cat_id)) +
     geom_bar(stat = "identity") +
     scale_x_discrete(drop = FALSE) +
     ggtitle ("Count of labels from prediction") +
     theme_bw() +
-    theme(axis.text.x = element_text(angle = 50, vjust = 0.5, hjust=0.35), legend.position = "none") +
-    ylim(0,65)
+    theme(axis.text.x = element_text(angle = 50, vjust = 0.5, hjust=0.35), legend.position = "none")
   
   #
   .GlobalEnv$model_matches_plot <- model_matches %>%
     count(pr_cat_id)
-  model_matches_plot[nrow(model_matches_plot) + 1,] <- c("Leaf100", 0)
-  model_matches_plot[nrow(model_matches_plot) + 1,] <- c("Leaf90", 0)
+  #model_matches_plot[nrow(model_matches_plot) + 1,] <- c("Leaf100", 0)
+  #model_matches_plot[nrow(model_matches_plot) + 1,] <- c("Leaf90", 0)
   model_matches_plot <- transform(model_matches_plot, n=as.numeric(n))
   
-  model_matches_plot$pr_cat_id <- factor(model_matches_plot$pr_cat_id, levels=c("Leaf100", "Leaf100UM", "Leaf100B", "Leaf100BUM", "Leaf90", "Leaf90UM", "Leaf50", "Leaf50UM"))
-  
+  model_matches_plot$pr_cat_id <- factor(model_matches_plot$pr_cat_id, levels=c(labels[1], labels[2], labels[3]#, labels[4]
+                                                                                ))
   p2 <- ggplot(model_matches_plot, aes(x=pr_cat_id, y=n, fill=pr_cat_id)) +
     geom_bar(stat = "identity") +
     scale_x_discrete(drop = FALSE) +
     ggtitle ("Count of labels that match same mapping") +
     theme_bw() +
-    theme(axis.text.x = element_text(angle = 50, vjust = 0.5, hjust=0.35), legend.position = "none") +
-    ylim(0,65)
+    theme(axis.text.x = element_text(angle = 50, vjust = 0.5, hjust=0.35), legend.position = "none")
   
   #
-  validation_meta_plot <- validation_meta %>% 
-    select(Leaf100, Leaf100B, Leaf90, Leaf50, id) %>% 
-    transform(Leaf100=as.numeric(Leaf100), Leaf90=as.numeric(Leaf90), Leaf100B=as.numeric(Leaf100B), Leaf50=as.numeric(Leaf50)) 
-  validation_meta_plot <- reshape2::melt(validation_meta_plot, variable.name = "pr_cat_id", value.name = "n")
-  validation_meta_plot <- validation_meta_plot %>% 
+  test_meta_plot <- test_meta %>% 
+    select(!!!labels, id) %>% 
+    transform(labels[1]=as.numeric(labels[1]), labels[2]=as.numeric(labels[2]), labels[3]=as.numeric(labels[3])#, labels[4]=as.numeric(labels[4])
+                                                                                                     )
+  test_meta_plot <- reshape2::melt(test_meta_plot, variable.name = "pr_cat_id", value.name = "n")
+  test_meta_plot <- test_meta_plot %>% 
     group_by(pr_cat_id) %>% 
     summarise(n = sum(n))
   
-  validation_meta_plot$pr_cat_id <- factor(validation_meta_plot$pr_cat_id, levels=c("Leaf100", "Leaf100UM", "Leaf100B", "Leaf100BUM", "Leaf90", "Leaf90UM", "Leaf50", "Leaf50UM"))
-  .GlobalEnv$validation_meta_plot
+  test_meta_plot$pr_cat_id <- factor(test_meta_plot$pr_cat_id, levels=c(labels[1], labels[2], labels[3]#, labels[4]
+                                                                        ))
+  .GlobalEnv$test_meta_plot
   
-  p3 <- ggplot(validation_meta_plot, aes(x=pr_cat_id, y=n, fill=pr_cat_id)) +
+  p3 <- ggplot(test_meta_plot, aes(x=pr_cat_id, y=n, fill=pr_cat_id)) +
     geom_bar(stat = "identity") +
     scale_x_discrete(drop = FALSE) +
     ggtitle ("Count of labels from groundtruth") +
     theme_bw() +
-    theme(axis.text.x = element_text(angle = 50, vjust = 0.5, hjust=0.35), legend.position = "none") +
-    ylim(0,65)
+    theme(axis.text.x = element_text(angle = 50, vjust = 0.5, hjust=0.35), legend.position = "none")
   
   ##  
   
@@ -113,10 +108,10 @@ machine_to_accuracy <- function(test, validation_meta_dir, model_summary_dir, mo
   ### 
   model_summary_count <- model_summary_plot  
   colnames(model_summary_count)<-c("pr_cat_id", "prediction")
-  validation_meta_count <- validation_meta_plot
-  colnames(validation_meta_count)<-c("pr_cat_id", "groundtruth")
+  test_meta_count <- test_meta_plot
+  colnames(test_meta_count)<-c("pr_cat_id", "groundtruth")
   
-  .GlobalEnv$difference <- inner_join(validation_meta_count, model_summary_count, by="pr_cat_id") %>% 
+  .GlobalEnv$difference <- inner_join(test_meta_count, model_summary_count, by="pr_cat_id") %>% 
     mutate(difference=groundtruth-prediction)
   
   #IoU
@@ -145,8 +140,8 @@ machine_to_accuracy <- function(test, validation_meta_dir, model_summary_dir, mo
     mutate(p_all = p_all)
   
   #recall -
-  r_all = sum(model_summary_count["prediction"]) / sum(validation_meta_count["groundtruth"])
-  .GlobalEnv$r_df_join <- merge(model_summary_count, validation_meta_count, by ='pr_cat_id') %>% 
+  r_all = sum(model_summary_count["prediction"]) / sum(test_meta_count["groundtruth"])
+  .GlobalEnv$r_df_join <- merge(model_summary_count, test_meta_count, by ='pr_cat_id') %>% 
     group_by (pr_cat_id) %>% 
     mutate (r = (sum(prediction) / sum(groundtruth))) %>% 
     mutate (r_all = r_all)
