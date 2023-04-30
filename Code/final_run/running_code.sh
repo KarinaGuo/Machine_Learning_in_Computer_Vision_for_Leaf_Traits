f410761a3c1a05136fbfead49952cc9f002ffd7a
KarinaGuo <100897883+KarinaGuo@users.noreply.github.com>
2023-04-30 13:22:20 +1000

    Update running_code.sh

--- Code/final_run/running_code.sh
+++ Code/final_run/running_code.sh
@@ -39,60 +39,72 @@ batch_list=`ls -d $PWD/image_subset_lists/*`
 echo "batches are" ${batch_list}
 
 # Iterate over each batch in the batch list
+# Iterate over each batch in the batch list
 for batch in $batch_list; do 
 	# Documentation of batch and date
-  echo "starting" ${batch} >> log.txt
+	echo "starting" ${batch} >> log.txt
 	now_time=`date`
 	echo $now_time >> log.txt
-  
-  # Activating conda environment
+
+	# Activating conda environment
 	conda activate pytorch
-	
-  # Create two temporary files to store the results of the leaf classification model
-  touch temp_filter.csv 
+
+	# Create two temporary files to store the results of the leaf classification model
+	touch temp_filter.csv 
 	touch temp_classifier_results_test.csv
 	echo "Making files: /temp_classifier_results_test.csv, /temp_filter.csv" 
-	
-  # Create a directory to store temporary images
+
+	# Create a directory to store temporary images
 	mkdir temp_image_subset 
 	echo "Making directories: /temp_image_subset"
 
-  # Create three directories to store temporary results from the leaf classification model
+	# Create three directories to store temporary results from the leaf classification model
 	mkdir temp_pred 
 	mkdir temp_pred 
 	mkdir temp_pred_leaf
 	mkdir temp_pred_leaf/subf 
 	echo "Making directories: /temp_pred, /temp_pred_leaf"
-	
-  # Loop over each image in the batch and copy it to the temporary image directory
-	for i in $(cat ${batch}); do echo copying ${i} >> log.txt; image=`basename ${i}`; ln -s ${i} temp_image_subset/$image; done 
+
+	# Loop over each image in the batch and copy it to the temporary image directory
+	for i in $(cat ${batch}); do 
+		echo copying ${i} >> log.txt; 
+		image=`basename ${i}`; 
+		ln -s ${i} temp_image_subset/$image; 
+	done 
 	echo "images copied"
-  
-  # Call a Python script to predict the leaf images from the leaf segmentation model
+
+	# Call a Python script to predict the leaf images from the leaf segmentation model
 	python /home/botml/code/dev/main_loop/predict_leaf.py $base_dir "main"
 	echo ${batch} "leaf dimension predicted" >> log.txt
 	echo "leaves predicted"
-  # Call a Python script to crop the leaf images
+
+	# Call a Python script to crop the leaf images
 	python /home/botml/code/dev/main_loop/extract_leaves_mt.py $base_dir "main" "Y"
 	echo ${batch} "leaves cropped" >> log.txt
 	echo "leaves cropped"
-  # Call a Python script to track duplicate leaf images from leaf segmentation model
-  python /home/botml/code/dev/main_loop/removing_duplicates.py $base_dir
-  echo ${batch} "duplicates tracked" >> log.txt
-  echo "duplicates tracked"
-  # Call a Python script to predict the class of the leaf images
+
+	# Call a Python script to track duplicate leaf images from leaf segmentation model
+	python /home/botml/code/dev/main_loop/removing_duplicates.py $base_dir
+	echo ${batch} "duplicates tracked" >> log.txt
+	echo "duplicates tracked"
+
+	# Call a Python script to predict the class of the leaf images
 	python /home/botml/code/dev/main_loop/predict_from_classifier.py $base_dir "main"
 	echo ${batch} "classifier predicted" >> log.txt
 	echo "classifier classed"  
-  # Call a Python script to remove invalid leaves
+
+	# Call a Python script to remove invalid leaves
 	python /home/botml/code/dev/main_loop/removing_files.py $base_dir
+
 	# Activate conda environment for trait extraction
-  conda activate MLpredictions 
-  # Call a Python script that calls the trait-extraction R script using multithreads
+	conda activate MLpredictions 
+
+	# Call a Python script that calls the trait-extraction R script using multithreads
 	python /home/botml/code/dev/main_loop/running_R_mt.py $base_dir
-  echo ${batch} "leaves traited" >> log.txt 
+	echo ${batch} "leaves traited" >> log.txt 
+
 	# Removing temporary files
-  rm -r temp*
+	rm -r temp*
 	echo ${batch} "removed temporary files" >> log.txt
 	echo ${batch} "completed!" >> log.txt
 	echo "batch completed :)"
